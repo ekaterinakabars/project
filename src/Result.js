f142b59cc86b223f526da7bb9eea170d96dd5033
ekaterinakabars <ekabariha123@gmail.com>
2021-10-14 19:21:17 +0300

    Finish

--- src/Result.js
+++ src/Result.js
@@ -3,18 +3,41 @@ import { MainContainer } from "./components/MainContainer";
 import { Table, Paper, TableContainer, Typography, TableHead, TableBody, TableRow, TableCell } from "@material-ui/core";
 import { useData } from "./DataContext";
 import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/styles";
+// import { PrimateButton } from "./components/PrimatButton";
+// import Confetti from 'confetti-react';
+
+
+// const Submit = () => {
+
+//     return <Confetti />;
+
+// }
+
+
+const useStyles = makeStyles({
+    root: {
+        marginBottom: "30px"
+    },
+    table: {
+        marginBottom: "30px"
+
+    }
+})
 
 export const Result = () => {
+
+    const styles = useStyles()
     const { data } = useData()
     const entries = Object.entries(data).filter((entry) => entry[0] !== "files")
-    const { files } = data;
+
     return (
         <MainContainer>
             <Typography component="h2" variant="h5">
                 📋 Form values
             </Typography>
-            <TableContainer component={Paper}>
-                <Table>
+            <TableContainer className={styles.root} component={Paper}>
+                <Table className={styles.table}>
                     <TableHead>
                         <TableRow>
                             <TableCell>
@@ -37,6 +60,11 @@ export const Result = () => {
                     </TableBody>
                 </Table>
             </TableContainer>
+
+            {/* <PrimateButton onClick={Submit}>Submit
+            </PrimateButton> */}
+
+
             <Link to="/">Start over </Link>
         </MainContainer>
     )
